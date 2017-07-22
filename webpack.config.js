da70e08544ab6bce27b4ac254bd0067973303ba1
potatoMa <potatomaa@163.com>
2017-07-22 15:25:19 +0800

    test

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+	entry:__dirname + "/app/js/main.js",
+	output:{
+		filename:"bundle.js",
+		path:__dirname + "/public"
+	},
+	//加载器
+	module:{
+		loaders:[{
+			test:/\.css$/,
+			loader:"style-loader!css-loader"
+		},{
+			test:/\.html$/,
+			loader:"html-loader"
+		},{
+			test:/\.(jpg|png|jepg|gif)$/,
+			loader:"url-loader"
+		},{
+			test:/\.js$/,
+			loader:"babel-loader"
+		},{
+			test:/\.vue$/,
+			loader:"vue-loader"
+		},{
+			test:/\.(woff|svg|eot|ttf)\??.*$/,
+			loader:"url-loader?name=fonts/[name].[md5:hash:hex:7].[ext]"
+		}]
+	},
+	resolve:{
+		alias:{
+			"vue":"vue/dist/vue.js"
+		}
+	},
+	devServer:{
+		contentBase:"./public",
+		inline:true,
+		port:7777
+	}
+}
